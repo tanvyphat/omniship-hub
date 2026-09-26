bebd51b1230ce5c5a8f955739dabb011d786cf21
Tân Vy Phát <tanvyphatpaper@gmail.com>
2026-09-26 15:08:10 +0700

    feat: mount feature flag provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './features/auth/useAuth';
+import { FeatureFlagProvider } from './features/featureFlags/useFeatureFlags';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <FeatureFlagProvider>
+          <App />
+        </FeatureFlagProvider>
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>,
